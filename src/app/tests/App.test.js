5f7f396924b0cc0b3a912c5af1944b325a3ba9f1
Musa-94 <x.m.m.o@mail.ru>
2021-02-08 01:36:19 +0400

    Merge remote-tracking branch 'origin/prod' into next-step
    # Conflicts:
    #	package.json
    #	src/app/App.jsx
    #	src/components/Header.jsx
    #	src/components/Header/Header.jsx
    #	src/components/Navbar.jsx
    #	src/components/Navbar/Navbar.jsx
    #	src/components/RecipesList.jsx
    #	src/components/RecipesList/RecipesList.jsx
    #	src/components/Search.jsx
    #	src/components/Search/Search.jsx
    #	src/components/header/Header.jsx
    #	src/components/navbar/Navbar.jsx
    #	src/components/recipesList/RecipesList.jsx
    #	src/components/search/Search.jsx
    #	src/pages/Default.jsx
    #	src/pages/Home/Home.jsx
    #	src/pages/Recipes/Recipes.jsx

--- src/app/tests/App.test.js
+++ src/app/tests/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from '../App.jsx';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App snapshot', () => {
+    it('should correct renderer', () => {
+        const component = renderer(<App/>);
+
+        expect(component).toMatchSnapshot();
+    });
 });
